a6a8a1f08793ce2894829a21a3cc424861f7c4fd
VgionTech <vgion.tech@gmail.com>
2021-06-10 21:50:52 +0530

    Locationmanager updated and much sorted.

--- app/src/main/java/com/example/location/LocationManager.java
+++ app/src/main/java/com/example/location/LocationManager.java
@@ -1,16 +1,20 @@
 package com.example.location;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Looper;
-import android.util.Log;
-import android.widget.Toast;
+import android.provider.Settings;
+import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -26,13 +30,14 @@ import com.google.android.gms.tasks.Task;
 public class LocationManager {
 
     private static LocationManager instance = null;
-    private LocationRequest locationRequest;
-    private LocationSettingsRequest.Builder locationSettingsRequestBuilder;
     private Context context;
+    private Activity activity;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private LocationSettingsCallBack locationSettingsCallBack;
-    private LocationCallback locationCallback;
     private Location lastLocation;
+    private static int REQUEST_CHECK_SETTINGS = 200;
+    private LocationRequest locationRequest;
+    private LocationCallback locationCallback;
+    private LocationTracker locationTracker;
 
     private LocationManager() {
     }
@@ -47,113 +52,131 @@ public class LocationManager {
 
     private void init(Context context) {
         this.context = context;
-        this.locationSettingsCallBack = (LocationSettingsCallBack) context;
-        setupLocationService();
-    }
+        this.activity = (Activity) context;
+        this.locationTracker = (LocationTracker) context;
+        this.fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
 
-    //call in init
-    void setupLocationService() {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context);
+        //continuous updates
         locationCallback = new LocationCallback() {
-            @Override public void onLocationResult(LocationResult locationResult) {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
                 if (locationResult == null) {
                     return;
                 }
-
                 for (Location location : locationResult.getLocations()) {
-                    Log.d("Location Update:",
-                            "Lat: " + location.getLatitude() + "Long: " + location.getLongitude());
                     // Update UI with location data
-                    // ...
+                    locationTracker.updateLocation(location);
                 }
             }
         };
+
+        createLocationRequest();
     }
 
-    //this function actually gets the location
-    //this function is called from onLocationSettingsSuccessful()
-    Location getLastLocation() {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) !=
-                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return null;
-        }
-        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-            @Override public void onSuccess(Location location) {
-                if (location == null) {
-                    Toast.makeText(context, "Could not fetch the location, Please try again!",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    lastLocation = location;
-                    Log.d("Last Location",
-                            "Lat: " + location.getLatitude() + "Long: " + location.getLongitude());
-                    //Toast.makeText(context,"Lat: " + location.getLatitude() + "Long: " +
-                    // location.getLongitude(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        return lastLocation;
-    }
-
-    //call this function on button click when location permissions are granted.
-    void createLocationRequest() {
+    //starts location on the device
+    protected void createLocationRequest() {
         locationRequest = LocationRequest.create();
         locationRequest.setInterval(10000);
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        getCurrentLocationSettings(locationRequest);
-    }
-
-    //this function is called in chain with createLocationRequest()
-    private void getCurrentLocationSettings(LocationRequest locationRequest) {
-        locationSettingsRequestBuilder =
+        LocationSettingsRequest.Builder builder =
                 new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
-        checkIfLocationSettingsAreOK();
-    }
-
-    //this function is called in chain with createLocationRequest() and getCurrentLocationSettings()
-    private void checkIfLocationSettingsAreOK() {
         SettingsClient client = LocationServices.getSettingsClient(context);
-        Task<LocationSettingsResponse> task =
-                client.checkLocationSettings(locationSettingsRequestBuilder.build());
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
 
-        task.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
-            @Override public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                locationSettingsCallBack.onLocationSettingsSuccessful();
+        task.addOnSuccessListener(activity, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
             }
         });
 
-        task.addOnFailureListener(new OnFailureListener() {
-            @Override public void onFailure(@NonNull Exception e) {
-                locationSettingsCallBack.onLocationSettingsFailure(e);
+        task.addOnFailureListener(activity, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    // Location settings are not satisfied, but this can be fixed
+                    // by showing the user a dialog.
+                    try {
+                        // Show the dialog by calling startResolutionForResult(),
+                        // and check the result in onActivityResult().
+                        ResolvableApiException resolvable = (ResolvableApiException) e;
+                        resolvable.startResolutionForResult(activity,
+                                REQUEST_CHECK_SETTINGS);
+                    } catch (IntentSender.SendIntentException sendEx) {
+                        // Ignore the error.
+                    }
+                }
             }
         });
     }
 
-    //call this function in onResume()
+    //gets last location
+    public Location getLastLocation() {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+        fusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(activity, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location == null) {
+                            startLocationUpdates();
+                            lastLocation = null;
+                        } else {
+                            lastLocation = location;
+                        }
+                    }
+                });
+        return lastLocation;
+    }
+
+    //call this in onResume
     public void startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,
+                locationCallback,
                 Looper.getMainLooper());
     }
 
-    //call this function in onPause()
+    //call this in onPause
     public void stopLocationUpdates() {
         fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 
-    //implement this in the activity/fragment
-    interface LocationSettingsCallBack {
-        //this function is called from checkIfLocationSettingsAreOK() and implemented in
-        // activity/fragment
-        void onLocationSettingsSuccessful();
+    //check if location is enabled on the device
+    public boolean isLocationEnabled() {
+        int locationMode = 0;
+        String locationProviders;
 
-        //this function is called from checkIfLocationSettingsAreOK() and implemented in
-        // activity/fragment
-        void onLocationSettingsFailure(Exception e);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            try {
+                locationMode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE);
+
+            } catch (Settings.SettingNotFoundException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+
+        }else{
+            locationProviders = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+            return !TextUtils.isEmpty(locationProviders);
+        }
+    }
+
+    //sends location to activity/fragment
+    interface LocationTracker {
+        void updateLocation(Location location);
     }
 }
